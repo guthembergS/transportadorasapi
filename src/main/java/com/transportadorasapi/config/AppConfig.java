488ef6c6c3843cb71a8f62a60131f0b68db6344a
guthembergS <gutheaugusto123@gmail.com>
2019-11-05 19:25:16 -0300

    ajustes de inicializacao/conexao

--- src/main/java/com/transportadorasapi/config/AppConfig.java
+++ src/main/java/com/transportadorasapi/config/AppConfig.java
@@ -12,6 +12,9 @@ import org.springframework.core.env.Environment;
 import org.springframework.orm.hibernate5.HibernateTransactionManager;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import static org.hibernate.cfg.AvailableSettings.HBM2DDL_AUTO;
+import static org.hibernate.cfg.AvailableSettings.SHOW_SQL;
 import static org.hibernate.cfg.Environment.*;
 
 @Configuration
@@ -37,7 +40,7 @@ public class AppConfig {
 		props.put(PASS,env.getProperty("mysql.password"));
 		
 		props.put(SHOW_SQL,env.getProperty("hibernate.show_sql"));
-		props.put(HBM2DDL_AUTO,env.getProperty("hibernate.hbm2ddl"));
+		props.put(HBM2DDL_AUTO,env.getProperty("hibernate.hbm2ddl.auto"));
 		
 		props.put(C3P0_MIN_SIZE,env.getProperty("hibernate.c3p0.min_size"));
 		props.put(C3P0_MAX_SIZE,env.getProperty("hibernate.c3p0.max_size"));
